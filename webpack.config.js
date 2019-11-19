af8f7616d40f938dbf19661450748c869663dc90
tbillings141 <26215390+tbillings141@users.noreply.github.com>
2019-11-19 14:12:54 -0600

    Setup webpack properly

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.svg$/, use: 'svg-inline-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({template: 'app/index.html'})
+  ]
+}
